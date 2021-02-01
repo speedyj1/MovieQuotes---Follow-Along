f2ce9756bea41d66e58ca4e39fb34c6ab73cf1eb
Jackson Speedy <speedyj1@rose-hulman.edu>
2021-02-01 00:18:08 -0500

    read and update users

--- MovieQuotes/ProfilePageViewController.swift
+++ MovieQuotes/ProfilePageViewController.swift
@@ -6,22 +6,30 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ProfilePageViewController: UIViewController {
     @IBOutlet weak var displayNameTextField: UITextField!
     @IBOutlet weak var profilePhotoImageView: UIImageView!
     
     override func viewDidLoad() {
+        UserManager.shared.beginListening(uid: Auth.auth().currentUser!.uid, changeListener: updateView)
         displayNameTextField.addTarget(self, action: #selector(handleNameEdit), for: .editingChanged)
     }
     
     @objc func handleNameEdit() {
         if let name = displayNameTextField.text {
             print("Name \(name)")
+            UserManager.shared.updateName(name: name)
         }
     }
     
     @IBAction func pressedEditPhoto(_ sender: Any) {
         print("Upload photo")
+    }
+    
+    func updateView() {
+        displayNameTextField.text = UserManager.shared.name
     }
 }
--- MovieQuotes/UserManager.swift
+++ MovieQuotes/UserManager.swift
@@ -45,8 +45,18 @@ class UserManager {
     
     
     //Read
-    func beginListening(uid: String, changeListener: () -> Void) {
-        
+    func beginListening(uid: String, changeListener: (() -> Void)? ) {
+        let userRef = _collectionRef.document(uid)
+        userRef.addSnapshotListener { (documentSnapshot, error) in
+            if let error = error {
+                print("Error listening for user \(error)")
+                return
+            }
+            if let documentSnapshot = documentSnapshot {
+                self._document = documentSnapshot
+                changeListener?()
+            }
+        }
     }
     
     func stopListening() {
@@ -55,11 +65,14 @@ class UserManager {
     
     //Update
     func updateName(name: String) {
+        let userRef = _collectionRef.document(Auth.auth().currentUser!.uid)
+        userRef.updateData([kKeyName: name])
         
     }
     
     func updatePhotoUrl(photoUrl: String) {
-        
+        let userRef = _collectionRef.document(Auth.auth().currentUser!.uid)
+        userRef.updateData([kKeyPhotoUrl: photoUrl])
     }
     
     //Delete - No delete
